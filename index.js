017a251e9c8ec86636ba0cb8ba4862e30b1ae62d
Jeremy Gobert <jeremy.gobert@student.ehb.be>
2021-11-12 11:14:30 +0100

    geen idee

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-xrctfvygubhnij
-cic i
+const express = require('express')
+const app = express()
+const port = 3000
 
+cosnt knex = require('knex')({
+    client: 'pg'
+    connection:{
+        host: 
+        
+    }
+})
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-yeet fucking kut slet ik snap er niks van
-ik nap er nogsteeds nik svna 
-ybvugsdv dsfvwnverwuibihbfunsvnjfvnjvprfvdpnhjrhbpp v hbihvehverphervh jerv phjvh pijv
-v
-v
-wev
-erw
-vev
-rer
-v
-bwtbrtjgnwpvweuw 0ev hweribvwubivweiub
-
-erctvygubhnijretvyg
-ectfyvghbjsxrdctfvg
-vygbuhnjk
-
-tvygbuhinjmk
-xrctvygbuhnijkml
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
